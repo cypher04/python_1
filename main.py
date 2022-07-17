bed4665746d2061c072a4def7f4658773575a485
seyi446@gmail.com <seyi446@gmail.com>
2022-07-16 17:54:38 -0700

    edited variable

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 kilometers = float(input("Enter value in kilometers: "))
 
 # conversion factor
-conv_fac = 0.621371
+conv = 0.621371
 
 # calculate miles
-miles = kilometers * conv_fac
+miles = kilometers * conv
 print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
