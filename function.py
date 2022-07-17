d9fa615ba2c7f5d3a39cd30f7939b7cf56e669d7
seyi446@gmail.com <seyi446@gmail.com>
2022-07-17 12:56:20 -0700

    added a new file, edited main for tutorial purpose

---
+++ function.py
@@ -0,0 +1,13 @@
+# function is a block of code which only runs when it is called. In python, identation with tabs or spaces is what is used
+
+def samehi(name):
+    print(f'hello {name}')
+samehi('igol')
+
+# return values
+def getSum(numb1, numb2):
+    total= numb1 + numb2
+    return total
+ans=getSum(90, 67)
+print(ans)
+
--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 
-# Taking kilometers input from the user
-kilometers = float(input("Enter value in kilometers: "))
+# Working with Dictionary
+person = {
+    'firstname':'Olumide',
+    'lastname':'maintain',
+}
 
-# conversion factor
-conv = 0.621371
+# to get a value
+print(person.get('firstname'))
 
-# calculate miles
-miles = kilometers * conv
-print('%0.2f kilometers is equal to %0.2f miles' % (kilometers, miles))
+# add key value
+person ['phone'] = 90888444777
+
+# to copy dict
+person2 = person.copy()
+
+# to delete items
+del(person2['phone'])
+person.pop('lastname')
+
+print(person2)
+
+print(person)
+
+# Getting Dict Keys
+print(dict.keys(person))
+
+# list of dictionaries
+people = [
+             {'name':'martha','age':30},
+    {'name': 'egbon','age':40}
+]
+
+print(people)
+
+# function is a block of code which only runs when it is called. In python, identation with tabs or spaces is what is used
+def same(name2='igok'):
+    print(f'hello{name2}')
+
