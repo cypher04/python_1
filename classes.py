801e0b0dbb7ad4e449f31f8c298507320e9aad95
seyi446@gmail.com <seyi446@gmail.com>
2022-07-19 13:13:19 -0700

    updated file with a new file called classes

---
+++ classes.py
@@ -0,0 +1,14 @@
+# classes are like blueprints for creating objects. An object has properties and methods (functions) associated
+# with it. Almost everything in python is an object
+
+class user:
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+
+# To intialize user object
+ife = user('seyi', 'seyi@gmail.com', 37)
+
+print(ife.name, ife.age, ife.email)
--- function.py
+++ function.py
@@ -128,3 +128,7 @@ print(greeting(name))
 
 
 
+
+
+
+
