ca4c925cf2257ed2112072b132221b21d160583d
seyi446@gmail.com <seyi446@gmail.com>
2022-07-21 04:55:24 -0700

    updated tuple file with a little tuple program

---
+++ tuple.py
@@ -0,0 +1,9 @@
+# tuples is a list that can't be changed or modified
+
+# the best data hto use with tuple is coordinates. This type of data doesn't need to change
+
+various = (4, 5, 6, 7)
+
+various[0] = 10
+
+print(various)
