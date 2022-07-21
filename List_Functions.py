6663bbb0a6dd71b6abd931223e93eac31980899f
seyi446@gmail.com <seyi446@gmail.com>
2022-07-20 22:03:15 -0700

    updated a List_Function file by adding description

--- List_Functions.py
+++ List_Functions.py
@@ -1,5 +1,7 @@
 # these are set of programs that show how to use a list function
 
+# You can check for the application of this program in my Sand_Box program (file)
+
 def ans():
     numbers = [2, 3, 4, 5, 6]
     return numbers
