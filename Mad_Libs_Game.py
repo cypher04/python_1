ccebb065aff457381c66bc39c6f524bfa80099ce
seyi446@gmail.com <seyi446@gmail.com>
2022-07-20 20:55:55 -0700

    updated Mad_libs file with a game

---
+++ Mad_Libs_Game.py
@@ -0,0 +1,16 @@
+# This is a simple matlib game
+
+print('Welcome player, lets see how you construct these words')
+print('the tile of this Game is : THE MAGIC COMPUTERS')
+
+first = input('Today, every student has a computer small enough to fit into his:')
+second = input('He can solve any math problem by simply pushing the computers little:')
+third = input('Computers can add, multiiply, divide, and:')
+fourth = input('They can also:')
+fifth = input('better than a human. Some computers are:')
+sixth = input('Others have a/an:')
+seventh = input('screen that shows all kinds of:')
+eight = input('and:')
+
+
+print('Today, every student has a computer small enough to fit into his:' + first + 'He can solve any math problem by simply pushing the computers little:' + second + 'Computers can add, multiiply, divide, and:' + third + 'They can also:' + fourth + 'better than a human. Some computers are:' + fifth + 'Others have a/an:' + sixth + 'screen that shows all kinds of:' + seventh + 'and:' + eight + 'figures')
