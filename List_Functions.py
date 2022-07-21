41d78106639744b061a9735e39fee6e622b7c1ef
seyi446@gmail.com <seyi446@gmail.com>
2022-07-20 21:56:34 -0700

    created Sand_Box file for random programs

---
+++ List_Functions.py
@@ -0,0 +1,17 @@
+# these are set of programs that show how to use a list function
+
+def ans():
+    numbers = [2, 3, 4, 5, 6]
+    return numbers
+print(ans())
+
+
+
+
+
+
+#friends = ['Lan', 'tobi', 'uzo', 'jay', 'ugo']
+
+#result = friends.extend(ans(numbers))
+
+#friends()
---
+++ Sand_Box.py
@@ -0,0 +1,12 @@
+# this is where i test random programs
+
+#this is a list function program, i created a custom module saved in (List_Function) and called it in this program
+
+from List_Functions import ans
+
+friends = ['tobi','ada', 'Emeka']
+
+friends.extend(ans())
+
+print(friends)
+
