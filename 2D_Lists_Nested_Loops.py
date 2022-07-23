7bfc32300abfbb6094f8fe82e4dec3e8063c3df8
seyi446@gmail.com <seyi446@gmail.com>
2022-07-22 19:45:18 -0700

    updated 2d file with code

--- 2D_Lists_Nested_Loops.py
+++ 2D_Lists_Nested_Loops.py
@@ -1,2 +1,14 @@
 # This is a 2d lists and Nested Loops program
 
+list = [
+    [0, 9, 8],
+    [7, 6, 5],
+    [4, 3, 2]
+]
+
+for row in list:
+    for col in row:
+        print(col)
+
+for row in list:
+        print(row)
